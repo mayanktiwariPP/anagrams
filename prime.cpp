e586d23dd248eaa0d3a309c3ec9a35b2df24859b
unknown <test@L-MAA-25004257.paypalcorp.com>
2015-07-22 10:16:10 +0530

    updates

--- prime.cpp
+++ prime.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include<cstdio>
-#include<cstring> 
 #include<cstdlib>
 #include <fstream>
-#include <string>
+#include <cstring>
 #include<strings.h>
 #define MAX 999
 #define LIMIT 10000000000000
@@ -13,20 +12,21 @@ using namespace std;
 int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
 
 
-struct sortedLetters {
+typedef struct sortedLetters {
      unsigned long long int value;
-     string word;
-};
+     const char* word;
+} sortedLetters;
 
-typedef struct sortedLetters* sortedLetterPtr;
-
-int compare (const void* a, const void* b)
-{
-
-    if ( ((sortedLetterPtr)a)->value <  ((sortedLetterPtr)b)->value ) return -1;
-    if ( ((sortedLetterPtr)a)->value == ((sortedLetterPtr)b)->value ) return 0;
-    if ( ((sortedLetterPtr)a)->value >  ((sortedLetterPtr)b)->value ) return 1;
-  return 0;
+int compare(const void * left, const void * right) {
+    const sortedLetters * a = (const sortedLetters *) left;
+    const sortedLetters * b = (const sortedLetters *) right;
+    if (a->value > b->value) {
+        return -1;
+    } else if (a->value < b->value) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 struct sortedLetters letters[MAX];
@@ -73,12 +73,16 @@ class ReadFile {
 		  				product *= primes[line[index]-97];
 				}   		
 		  }
-	      cout << line << "\t" << product <<endl;
+	      //cout << line << "\t" << product <<endl;
 	      
+	      char * a = new char[line.size()+1];
+	      a[line.size()]=0;
+	      memcpy(a,line.c_str(),line.size());
 	      //harsha's code
-	      letters[linesOfFile].word = line;
+	      letters[linesOfFile].word = a;
           letters[linesOfFile].value = product;
-		
+		  
+		  //cout << letters[linesOfFile].word << "\t" << letters[linesOfFile].value <<endl;
 		}
 	} 
 	else 
@@ -95,9 +99,11 @@ int main ()
 	crossword.MapFile();
 	//long size = crossword.getFileSize();
 	
+	//qsort (letters, linesOfFile, sizeof(unsigned long long int), compare);
+	
 	for(int i=0;i<linesOfFile; i++) {
 		cout << letters[i].word <<"\t" << letters[i].value <<endl;
 	}
 	
-	//qsort (letters, linesOfFile, sizeof(unsigned long long int), compare);
+	
 }
